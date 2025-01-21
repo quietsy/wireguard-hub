abb328388e6415da8488ffccac5758ede4d074c8
quietsy <github@qtsy.cc>
2025-01-21 12:05:56 +0200

    Cleanup

--- wireguard/unblock/index.php
+++ wireguard/unblock/index.php
@@ -1,29 +1,28 @@
-<!DOCTYPE html> 
-<html> 
-<head> 
-  <title>Unblock</title> 
-</head> 
-<body style="background-color:black;font-size:34pt;color:white;"> 
-<center>
-<form method="post" style="padding-top:10%;"> 
-  <input type="text" name="URL" id="URL" style="height:80px;font-size:34pt;"><br><br>
-  <button type="submit" name="Unblock" style="height:80px;font-size:34pt;">Unblock</button> 
-</form> 
-<?php
-if(isset($_POST['Unblock'])) { 
-	$url = $_POST['URL'];
-	$file = 'domains.txt';
-	$parsed = parse_url($url, PHP_URL_HOST) ?: $url;
-	$current = file_get_contents($file);
-	if (str_contains($current, $parsed)) {
-		echo $parsed.' is already unblocked';
-	} else {
-		file_put_contents($file, PHP_EOL.$parsed, FILE_APPEND | LOCK_EX);
-		echo 'Unblocked '.$parsed;
-	}
-} 
-?> 
-</center>
- 
-</body> 
-</html> 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Unblock</title>
+	</head>
+	<body style="background-color:black;font-size:34pt;color:white;">
+		<center>
+			<form method="post" style="padding-top:10%;">
+				<input type="text" name="URL" id="URL" style="height:80px;font-size:34pt;"><br><br>
+				<button type="submit" name="Unblock" style="height:80px;font-size:34pt;">Unblock</button>
+			</form>
+			<?php
+			if(isset($_POST['Unblock'])) {
+				$url = $_POST['URL'];
+				$file = 'domains.txt';
+				$parsed = parse_url($url, PHP_URL_HOST) ?: $url;
+				$current = file_get_contents($file);
+				if (str_contains($current, $parsed)) {
+					echo $parsed.' is already unblocked';
+				} else {
+					file_put_contents($file, PHP_EOL.$parsed, FILE_APPEND | LOCK_EX);
+					echo 'Unblocked '.$parsed;
+				}
+			}
+			?>
+		</center>
+	</body>
+</html>
